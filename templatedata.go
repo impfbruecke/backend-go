62b103a08493ff1685ca34b6d8ea608080d74ed9
Pablo Ovelleiro Corral <github@pablo.tools>
2021-02-12 15:58:22 +0100

    add comment to templatedata

--- templatedata.go
+++ templatedata.go
@@ -1,5 +1,9 @@
 package main
 
+// TmplData bundles the data that is passed to the html templates for easier
+// and uniform acces to it. The fields which are not used in a certain template
+// may be nil, it is up to the template to check for nil values where they
+// might occurr
 type TmplData struct {
 	CurrentUser        string
 	DefaultCapacity    string
