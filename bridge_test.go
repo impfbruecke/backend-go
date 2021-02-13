e16adce7e62ff71ef8e1211692e7978bded60e74
Pablo Ovelleiro Corral <github@pablo.tools>
2021-02-13 16:35:15 +0100

    add test for addPersons()

--- bridge_test.go
+++ bridge_test.go
@@ -146,6 +146,7 @@ func TestBridge_GetAcceptedPersons(t *testing.T) {
 }
 
 func TestBridge_AddPerson(t *testing.T) {
+	prepareTestDatabase()
 	tests := []struct {
 		name    string
 		person  Person
@@ -188,29 +189,42 @@ func TestBridge_AddPerson(t *testing.T) {
 }
 
 func TestBridge_AddPersons(t *testing.T) {
-	type fields struct {
-		db     *sqlx.DB
-		sender *TwillioSender
-	}
-	type args struct {
-		persons []Person
-	}
+	prepareTestDatabase()
 	tests := []struct {
 		name    string
-		fields  fields
-		args    args
+		persons []Person
+		want    []Person
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Add two persons",
+			persons: []Person{
+				{"0001", 0, 1, false},
+				{"0002", 0, 1, false},
+			},
+			want: []Person{
+				{Phone: "1230", Group: 1},
+				{Phone: "1231", Group: 1},
+				{Phone: "1232", Group: 1},
+				{"0001", 0, 1, false},
+				{"0002", 0, 1, false},
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			b := &Bridge{
-				db:     tt.fields.db,
-				sender: tt.fields.sender,
-			}
-			if err := b.AddPersons(tt.args.persons); (err != nil) != tt.wantErr {
+			if err := bridge.AddPersons(tt.persons); (err != nil) != tt.wantErr {
 				t.Errorf("Bridge.AddPersons() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			got, err := bridge.GetPersons()
+			if err != nil {
+				t.Errorf("GetPersons() after AddPersons() failed with error: %v \n", err)
+			}
+
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("GetPersons() after AddPersons() mismatch (-want +got):\n%s", diff)
 			}
 		})
 	}
